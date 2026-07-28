d14b4bd9eefdb699dcf5013b27b0aa1019884b3e
Peter Brown <brown.peterg@gmail.com>
2026-07-28 19:18:49 -0400

    eslint, vite, infra setup; logic for world rendering

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,11 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
 ];
--- main.js
+++ main.js
@@ -4,42 +4,45 @@ const chunkWidth = 36;
 const viewportWidth = window.innerWidth;
 const terrainTypes = [
   {
-    symbol: "[ → ]",
+    symbol: '[ → ]',
     left: 0,
     right: 0,
   },
   {
-    symbol: "[ / ]",
+    symbol: '[ / ]',
     left: 0,
     right: 1,
   },
   {
-    symbol: "[ ⁻ ]",
+    symbol: '[ ⁻ ]',
     left: 1,
     right: 1,
   },
   {
-    symbol: "[ \\ ]",
+    symbol: '[ \\ ]',
     left: 1,
     right: 0,
   },
 ];
+
 const terrainContainer = document.querySelector('#terrain');
 const chunksVisible = Math.ceil(viewportWidth / chunkWidth);
 
 function buildWorld(world, chunksVisible, terrainTypes) {
   world.push(terrainTypes[Math.floor(Math.random() * terrainTypes.length)]);
   let i = 0;
-  while (world.length < (chunksVisible + (buffer * 2))) {
-    for (let x = 0; i <= world.length; i++) {
-      if (terrainTypes[x].left === world[i].right {
+  while (world.length < chunksVisible + (buffer * 2)) {
+    console.log('inside while, i: ' + i);
+    for (let x = 0; x < terrainTypes.length; x++) {
+      console.log('inside while and for, x: ' + x)
+      console.log('inside while and for, i: ' + i);
+      if (terrainTypes[x].left === world[i].right) {
         world.push(terrainTypes[x]);
       }
     }
     i++;
-
-    // world.push(terrainTypes[Math.floor(Math.random() * terrainTypes.length)]);
   }
+
   terrainContainer.append(
     world.map(chunk => chunk.symbol).join('')
   );
@@ -58,3 +61,4 @@ document.addEventListener('keydown', (event) => {
 window.addEventListener('resize', () => buildWorld(world, chunksVisible, terrainTypes));
 
 buildWorld(world, chunksVisible, terrainTypes);
+
